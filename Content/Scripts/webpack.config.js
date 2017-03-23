2ae05b979fb7900975bbcae598f31f70d3146e00
crocuis <crocuis@gmail.com>
2017-03-24 01:46:47 +0900

    update for webpack2

--- Content/Scripts/webpack.config.js
+++ Content/Scripts/webpack.config.js
@@ -1,48 +1,53 @@
 var webpack = require('webpack');
-var UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
 var env = process.env.WEBPACK_ENV || 'dev';
 var path = require('path');
-var NpmInstallPlugin = require("npm-install-webpack-plugin");
 
 var appName = 'index';
 
-var plugins = [], outputFile;
-
-if (env === 'build') {
-  plugins.push(new UglifyJsPlugin({ minimize: true }));
-  outputFile = appName + '.min.js';
-} else {
-  outputFile = appName + '.js';
-}
+var plugins = [], outputFile = appName + '.js';
 
 var config = {
   entry: './src/index.jsx',
+  target: 'node',
   output: {
     path: __dirname + '/build',
     filename: outputFile,
     libraryTarget: 'commonjs2'
   },
   module: {
-    loaders: [
+    rules: [
       {
         test: /(\.jsx|\.js)$/,
-        loader: 'babel-loader',
-        exclude: /(node_modules|bower_components)/,
-        query: {
-          presets: ['react', 'es2015']
-        }
+        exclude: /(node_modules|bower_components)/,        
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                  [ 'es2015', { modules: false } ],
+                  'react' 
+                ]
+
+            }
+          }
+        ]
       }
     ]
   },
   resolve: {
-    root: path.resolve('./src'),
-    extensions: ['', '.js', '.jsx']
+    extensions: ['.js', '.jsx'],   
+    modules: [
+      path.join(__dirname, "src"),
+      "node_modules"
+    ],
   },
-  plugins: [...plugins, 
-            new NpmInstallPlugin(),
+  plugins: [...plugins,            
+            new webpack.LoaderOptionsPlugin({ 
+              minimize: true, debug: false 
+            }),  
             new webpack.DefinePlugin({
               'process.env': {
-                NODE_ENV: JSON.stringify('production')
+                NODE_ENV: JSON.stringify(process.env.NODE_ENV)
               }
             })
            ]
